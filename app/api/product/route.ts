227bf01fabdefbc7a62077129e02de9d2b7154e5
Halid Ünal <halidunal@gmail.com>
2025-01-09 17:45:30 +0300

    product model and service

---
+++ app/api/product/route.ts
@@ -0,0 +1,25 @@
+import { getUser } from "@/app/actions/userActions";
+import { prisma } from "@/libs/prisma"
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+	const currentUser = await getUser();
+
+	if (!currentUser || currentUser.role !== "ADMIN") {
+		return NextResponse.error()
+	}
+	const body = await req.json();
+	const { name, description, brand, category, price, inStock, image } = body;
+	const product = await prisma.product.create({
+		data: {
+			name,
+			description,
+			brand,
+			category,
+			price: parseFloat(price),
+			inStock,
+			image
+		}
+	})
+	return NextResponse.json(product)
+}
